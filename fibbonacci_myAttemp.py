868fcb00504b27df892c6ce766ea29723eddbcc4
surajit <surajit.barad@gmail.com>
2021-11-29 22:06:43 +0530

    Formatted Code

--- fibbonacci_myAttemp.py
+++ fibbonacci_myAttemp.py
@@ -1,15 +1,24 @@
 def fibb(n):
-    num = [0,1]
-    if n>=2:
-        for i in range(2,n):
-            number = num[i-1] + num[i-2]
+    """Fibbonacci Number
+
+    Args:
+        n ([int]): [position fibbonacci number]
+
+    Returns:
+        [int]: [nth fibbonacci number]
+    """
+    num = [0, 1]
+    if n >= 2:
+        for i in range(2, n):
+            number = num[i - 1] + num[i - 2]
             num.append(number)
         return num[-1]
     else:
-        if n==1:
+        if n == 1:
             return 1
         else:
             return 0
 
-print(fibb(9))
-    
+
+if __name__ == "__main__":
+    print(fibb(9))
